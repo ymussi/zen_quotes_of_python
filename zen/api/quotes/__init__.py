54edb9cd01393a9ca3b473529ac3ede7124cea93
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2020-02-24 20:31:56 -0300

    more validations

--- zen/api/quotes/__init__.py
+++ zen/api/quotes/__init__.py
@@ -5,7 +5,9 @@ class Quotes():
         self.langs = Config.get('quotes', 'langs').split(',')
 
     def get_quotes(self, lang = None, number = None):
-        if lang is None:
+        if number is not None and int(number) > 19:
+            resp = 'Citation referring to the number informed not found. The Zen of Python has a total of 19 quotes.'
+        elif lang is None:
             quotes = Config.get('quotes', 'zen_en')
             resp = {"quotes": [el.strip() for el in quotes.split('/')] if number is None else quotes.split('/')[int(number)-1].strip()}
         elif lang in self.langs:
