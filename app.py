3a3f0db827331bddaa3a396534bc0fc2fdee1b10
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2020-02-25 19:00:50 -0300

    procfile

--- app.py
+++ app.py
@@ -2,25 +2,16 @@ from flask import Flask
 from flask_graphql import GraphQLView
 from zen.api.quotes.schemas import schema
 
-def create_app():
+app = Flask('__name__')
 
-    app = Flask('graphql')
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
+    "graphql",
+    schema=schema,
+    graphiql=True
+))
 
-    app.add_url_rule('/graphql', view_func=GraphQLView.as_view(
-        "graphql",
-        schema=schema,
-        graphiql=True
-    ))
-
+if __name__ == "__main__":
     host = '0.0.0.0'
-    # port = '8888'
+    port = '8888'
     debug = True
     app.run(host, int(port), debug)
-
-    return app
-
-# if __name__ == "__main__":
-#     host = '0.0.0.0'
-#     port = '8888'
-#     debug = True
-#     app.run(host, int(port), debug)
